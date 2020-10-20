44d60e2bedd66301e6c6c046e59ea8911a058494
jeshna05 <60674488+jeshna05@users.noreply.github.com>
2020-10-20 13:57:45 -0500

    Navigation Bar Code

---
+++ camera.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+      padding: EdgeInsets.all(25.0),
+      child: Text('Translate', style: TextStyle(fontSize: 36.0),),
+    );
+  }
+}
+
---
+++ history.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+      padding: EdgeInsets.all(25.0),
+      child: Text('History', style: TextStyle(fontSize: 36.0),),
+    );
+  }
+}
